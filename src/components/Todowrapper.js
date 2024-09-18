4be373e76c522f546e1cfbcdc570bb8f8433967b
GODSWILL <idolorwilly@icloud.com>
2024-09-18 19:00:31 +0100

    first commit

---
+++ src/components/Todowrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TodoForm } from './TodoForm'
+
+export const Todowrapper = () => {
+  return (
+    <div className='Todowrapper'>
+        <TodoForm/>
+    </div>
+  )
+}
